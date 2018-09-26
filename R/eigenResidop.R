19a6ab50f331ba36573daa2be16b38b6081b8538
kevinwang09 <kevin.wang@sydney.edu.au>
2018-09-26 17:27:12 +1000

    v0.1.10, suggestions from wc

--- R/eigenResidop.R
+++ R/eigenResidop.R
@@ -5,12 +5,12 @@
 #' @export
 
 eigenResidop = function(A, B){
-  
+
   tBB = eigenMatMult(t(B), B)
   tBA = eigenMatMult(t(B), A)
   tBB_inv = eigenMatInverse(tBB)
   BtBB_inv = eigenMatMult(B, tBB_inv)
   BtBB_inv_tBA = eigenMatMult(BtBB_inv, tBA)
-  
+
   return(A - BtBB_inv_tBA)
 }
--- src/rcppFastMatrixOp.cpp
+++ src/rcppFastMatrixOp.cpp
@@ -2,17 +2,24 @@
 
 #include <RcppEigen.h>
 
-
+//' @title fast matrix multiplication
+//' @param A a matrix
+//' @param B a matrix
+//' @export
 // [[Rcpp::export]]
 SEXP eigenMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
   Eigen::MatrixXd C = A * B;
-  
+
   return Rcpp::wrap(C);
 }
 
+
+//' @title fast matrix inverse
+//' @param A a matrix
+//' @export
 // [[Rcpp::export]]
 SEXP eigenMatInverse(const Eigen::Map<Eigen::MatrixXd> A){
   Eigen::MatrixXd Ainv = A.inverse();
-  
+
   return Rcpp::wrap(Ainv);
 }
