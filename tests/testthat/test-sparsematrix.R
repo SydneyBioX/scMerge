622e2c043e03e2fa76cfba3959caccc3400aa947
Kevin Wang <kevin.wang@sydney.edu.au>
2019-09-11 17:36:33 +1000

    added sparse matrix RcppEigen functions

--- tests/testthat/test-sparsematrix.R
+++ tests/testthat/test-sparsematrix.R
@@ -3,7 +3,7 @@ context("Test sparseArray")
 library(Matrix)
 
 set.seed(12345)
-L = ruvSimulate(m = 100, n = 1000, nc = 400, 
+L = ruvSimulate(m = 500, n = 20000, nc = 400, 
                 nCelltypes = 3, nBatch = 2, 
                 lambda = 0.1, sce = TRUE)
 
@@ -33,3 +33,49 @@ sce_sp <- scMerge(
 
 expect_identical(as.matrix(assay(sce_sp, "sp_output")), 
                  assay(sce_matrix, "matrix_output"))
+
+#################################################################
+
+L = ruvSimulate(m = 1000, n = 20000, nc = 400, 
+                nCelltypes = 3, nBatch = 2, 
+                lambda = 0.1, sce = FALSE)
+Y = L$Y
+M = L$M
+Y_sp = as(Y, "dgCMatrix")
+M_sp = as(M, "dgCMatrix")
+mean(Y == 0)
+mean(M == 0)
+
+sp_residop = function (A, B)
+{
+  return(A - B %*% base::solve(t(B) %*% B) %*% t(B) %*% A)
+}
+
+# microbenchmark::microbenchmark(
+#   t(Y) %*% M,
+#   eigenMatMult(t(Y), M),
+#   t(Y_sp) %*% M_sp,
+#   eigenSpMatMult(t(Y_sp), M_sp),
+#   times = 20)
+
+# microbenchmark::microbenchmark(
+#   ruv::residop(Y, M),
+#   eigenResidop(Y, M),
+#   eigenSpResidop(Y_sp, M_sp),
+#   sp_residop(Y_sp, M_sp),
+#   times = 5)
+
+profvis::profvis({
+  ruv::residop(Y, M)
+  eigenResidop(Y, M)
+  sp_residop(Y_sp, M_sp)
+  eigenSpResidop(Y_sp, M_sp)
+})
+
+
+profvis::profvis({
+  t(Y) %*% M
+  eigenMatMult(t(Y), M)
+  t(Y_sp) %*% M_sp
+  eigenSpMatMult(t(Y_sp), M_sp)
+})
