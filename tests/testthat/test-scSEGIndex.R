60f97bc3cc59a051c1149fac028c0eae4fd7ab9b
Kevin Wang <kevin.wang@sydney.edu.au>
2019-09-19 17:20:38 +1000

    added celltype option to test-scSEGIndex

--- tests/testthat/test-scSEGIndex.R
+++ tests/testthat/test-scSEGIndex.R
@@ -1,6 +1,8 @@
+context("Testing scSEGIndex function")
 ## Loading example data
 data('example_sce', package = 'scMerge')
 ## subsetting genes to illustrate usage.
 exprs_mat = SummarizedExperiment::assay(example_sce, 'logcounts')[1:110, 1:20]
 set.seed(1)
 scSEGIndex(exprs_mat = exprs_mat)
+scSEGIndex(exprs_mat = exprs_mat, cell_type = example_sce$cellTypes[1:20])
